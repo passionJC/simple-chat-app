a6439f59ace3c8fa72cbc59aa4ae1068d48391db
jaycepogi <jaycepogi53@gmail.com>
2023-05-05 13:39:17 +0800

    second commit

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "http://passionJC.github.io/simple-chat-app",
         methods: ["GET", "POST"],
     },
 });
@@ -31,6 +31,6 @@ io.on("connection", (socket) => {
     });
 });
 
-server.listen(3001, () => {
+server.listen("http://passionJC.github.io/simple-chat-app", () => {
     console.log("SERVER RUNNING");
 });
